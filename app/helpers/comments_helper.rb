dd89a7b049533a343e832fea01537b56ef096cc9
Lucas Padovan <zerobba8@hotmail.com>
2012-07-10 20:20:52 -0300

    usando nuevamente la consulta de los ultimos comentarios vieja, por ser mas eficiente en el servidor

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,13 @@
 module CommentsHelper
   def ultimos_comentarios
-    Comment.find_by_sql("SELECT DISTINCT ON (c.id)
-           u.username, co.user_id, c.title, co.conversation_id, co.body, co.id, co.created_at
-           FROM conversations c, comments co, users u
-           WHERE c.id = co.conversation_id AND u.id = co.user_id
-           ORDER BY c.id DESC, co.created_at DESC
-           LIMIT 10;")
+    comentarios_unicos = []
+    Comment.all.reverse_each do |comment|
+      unless comentarios_unicos.find{|c| c.conversation_id == comment.conversation_id}
+        comentarios_unicos << comment
+      end
+      break if comentarios_unicos.size > 10
+    end
+    comentarios_unicos
   end
 end
 
