17cd90ab61d8c6697b286ef0130e247cdab864b0
Lucas Padovan <zerobba8@hotmail.com>
2012-07-10 18:05:39 -0300

    Nueva consulta sql para ultimos comentarios

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,19 +1,11 @@
 module CommentsHelper
   def ultimos_comentarios
-    Comment.find_by_sql("SELECT co.body, co.id, u.username, co.user_id, c.title, co.conversation_id, co.created_at
-    FROM users u, conversations c,
-    (WITH conversations AS (
-          SELECT c.id,
-    	   c.user_id,
-               c.body,
-               c.conversation_id,
-               c.created_at,
-               ROW_NUMBER() OVER(PARTITION BY c.conversation_id
-                                     ORDER BY c.created_at DESC) AS rk
-          FROM comments c)
-      SELECT s.*  FROM conversations s WHERE s.rk = 1) AS co
-    WHERE co.conversation_id = c.id AND co.user_id = u.id
-    ORDER BY co.created_at DESC LIMIT 10; ")
+    Comment.find_by_sql("SELECT DISTINCT ON (c.id)
+           u.username, co.user_id, c.title, co.conversation_id, co.body, co.id, co.created_at
+           FROM conversations c, comments co, users u
+           WHERE c.id = co.conversation_id AND u.id = co.user_id
+           ORDER BY c.id DESC, co.created_at DESC
+           LIMIT 10;")
   end
 end
 
